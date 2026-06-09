40fdbb57a7d41e802efacb9765bcb3435718a9cc
cerealandchoccymilk <85705148+cerealandchoccymilk@users.noreply.github.com>
2026-06-08 22:18:42 -0400

    New bog post Also tidied up button wall, add bog TOC script, update humans.txt

---
+++ public/js/auto-toc-ol.js
@@ -0,0 +1,26 @@
+initTableOfContents();
+
+function initTableOfContents() {
+	const container = document.querySelector("#toc");
+	if (!container) return;
+
+	const allHeadings = document.querySelectorAll("h3");
+	if (allHeadings.length < 2) return;
+	let output = "<h2 class='h2top'>Page navigation</h2><ol>";
+	[...allHeadings].forEach((headingEl) => {
+		const title = headingEl.innerHTML;
+		const link =
+			headingEl.getAttribute("id") ||
+			encodeURI(
+				title
+					.replaceAll(" ", "-")
+					.replaceAll("#", "")
+					.replaceAll("&", "")
+					.replaceAll(/<[^>]*>?/gm, "")
+					.replaceAll("--", "-")
+			).toLowerCase();
+		headingEl.setAttribute("id", link);
+		output += `<li><a href="#${link}">${title}</a></li>`;
+	});
+	container.innerHTML = output + "</ol>";
+}
